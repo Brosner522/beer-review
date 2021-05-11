654199f2bf9a3ce7aab43730791fc80b3a3fcc6d
Ryan Andrews <rcandrews44@gmail.com>
2021-05-11 11:30:56 -0400

    new component

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("e2f200350fmsh95ead331d49f3dbp1075c9jsncabcb8d5806e")
+    fetch("http://localhost:3001/beers")
       .then(res => res.json())
       .then(data => console.log(data))
+    // this.setState({
+    //   beers: beers
+
   }
 
   render() {
--- src/Components/BeerContainer.js
+++ src/Components/BeerContainer.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import BeerCard from './BeerCard'
 
 class BeerContainer extends Component {
     render() {
         return (
             <div className={'beerContainer'}>
                 <h1>I am here</h1>
-                {this.props.beers.map()}
+                {this.props.beers.map(beer => <BeerCard beerObj={beer} />)}
             </div>
         )
     }
